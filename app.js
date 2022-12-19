7dd2d297c89184fc48ae2566fca92b901a2b7fa1
Vishal Kumar <5060vishalkumar@gmail.com>
2022-12-19 23:35:00 +0530

    version 1

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
-var ip = require('ip');
+const IP = require('ip');
 
 app.get('/' ,(req ,res)=>{
-    console.log(ip.address())
-    res.send(ip.address())
+    const ipAddress = IP.address();
+    res.send(ipAddress)
+
 })
+
+app.get('/express' ,(req ,res)=>{
+    const ipAddress = req.socket.remoteAddress;
+    res.send(ipAddress);
+
+})
+
+app.get('/header' ,(req ,res)=>{
+    const ipAddresses = req.header('x-forwarded-for');
+    res.send(ipAddresses);
+
+})
+
+
 app.listen(3000,()=>{
     console.log(`The server is listen on the port no http://localhost:${3000}`)
 })
